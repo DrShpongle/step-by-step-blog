c61224983e91bb3783f054e6deb8f00b4f73de96
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-19 21:08:16 +0200

    move PageLayout on the page level

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -7,7 +7,9 @@ const PageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow py-8 md:py-12 lg:py-16 xl:py-20">
+        {children}
+      </main>
       <Footer />
     </div>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-import PageLayout from '../components/layouts/page-layout'
 
 function MyApp({Component, pageProps}: AppProps) {
-  return (
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
