5e098a98a5740c74076d68f74ea054389296397c
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-20 09:17:01 +0200

    base url

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
-import Header from '../app/header'
-import Footer from '../app/footer'
+import Header from 'components/app/header'
+import Footer from 'components/app/footer'
 
 const PageLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
   return (
