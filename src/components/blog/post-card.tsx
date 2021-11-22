57c10225386421b832f38af56bb1753a8b3e7cd5
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-22 13:48:55 +0200

    fix padding

--- src/components/blog/post-card.tsx
+++ src/components/blog/post-card.tsx
@@ -12,7 +12,7 @@ export const PostCard: React.FC<PostCardProps> = ({post}) => {
   const {meta, slug} = post
   const {title, excerpt, date, postImage} = meta
   return (
-    <div className="flex flex-col py-4 pl-4 pr-10 space-y-4 md:space-y-0 md:space-x-10 md:flex-row rounded-xl bg-viridian-blue-dark">
+    <div className="flex flex-col py-4 pl-4 pr-4 space-y-4 md:pr-10 md:space-y-0 md:space-x-10 md:flex-row rounded-xl bg-viridian-blue-dark">
       {postImage?.url && (
         <div className="relative flex-shrink-0 w-full h-48 overflow-hidden md:w-64 rounded-xl">
           <Link href={`/blog/posts/${slug}`}>
