8649df9b4614cebf8167365b87406f08664ada2c
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2021-11-22 15:02:08 +0200

    bulletproof themes toggliong

--- src/components/dark-mode-toggler.tsx
+++ src/components/dark-mode-toggler.tsx
@@ -3,9 +3,7 @@ import {useTheme} from 'next-themes'
 import {motion} from 'framer-motion'
 
 const DarkModeToggler = () => {
-  const [isMounted, setIsMounted] = React.useState<boolean>(false)
   const {theme, setTheme} = useTheme()
-  React.useEffect(() => setIsMounted(true), [])
   const handleClick = () => {
     const nextTheme = theme === 'dark' ? 'light' : 'dark'
     setTheme(nextTheme)
@@ -19,48 +17,46 @@ const DarkModeToggler = () => {
           aria-label="Toggle Dark Mode"
           role="button"
         >
-          {isMounted ? (
-            <motion.div
-              variants={{
-                hidden: {opacity: 0, x: theme === 'dark' ? '24px' : 0},
-                light: {
-                  x: 0,
-                  opacity: 1,
-                },
-                dark: {
-                  x: '24px',
-                  opacity: 1,
-                },
-              }}
-              animate={theme === 'dark' ? 'dark' : 'light'}
-              initial="hidden"
-              className="flex items-center justify-center w-8 h-8 rounded-full shadow-md bg-champagne dark:bg-viridian-blue-darkest"
+          <motion.div
+            variants={{
+              hidden: {x: theme === 'dark' ? '24px' : 0},
+              light: {
+                x: 0,
+                opacity: 1,
+              },
+              dark: {
+                x: '24px',
+                opacity: 1,
+              },
+            }}
+            animate={theme === 'dark' ? 'dark' : 'light'}
+            initial="hidden"
+            className="flex items-center justify-center w-8 h-8 rounded-full shadow-md bg-champagne dark:bg-viridian-blue-darkest"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              className="w-4 h-4 text-viridian-blue-darkest dark:text-champagne"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                stroke="currentColor"
-                className="w-4 h-4 text-viridian-blue-darkest dark:text-champagne"
-              >
-                {theme === 'dark' ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                )}
-              </svg>
-            </motion.div>
-          ) : null}
+              {theme === 'dark' ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              )}
+            </svg>
+          </motion.div>
         </div>
       </div>
     </div>
